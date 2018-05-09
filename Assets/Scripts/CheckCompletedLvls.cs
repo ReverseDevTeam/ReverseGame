f177885f8f5bef56be9335f7de5fe03acf4c2a22
ninjawarrior1337 <t.rothled@gmail.com>
2018-05-09 16:29:33 -0700

    added lvl2

---
+++ Assets/Scripts/CheckCompletedLvls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckCompletedLvls : MonoBehaviour {
+
+	public List<Button> lvlButtons = new List<Button>();
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 1; i <= 6; i++) 
+		{
+			lvlButtons.Add((GameObject.Find("/Canvas/fd/LevelSelect/" + i).GetComponent<Button>()));
+		}
+
+		lvlButtons[0].interactable = true;
+
+		for (int i = 1; i <= 6; i++) 
+		{
+			if (PlayerPrefs.GetInt ("lvl" + i + "_complete") == 1) {
+				lvlButtons [i].interactable = true;
+			}
+			else 
+			{
+				lvlButtons [i].interactable = false;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
